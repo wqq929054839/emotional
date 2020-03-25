cd4b4c19e8935813e486b98d7507ce4e150fcfbe
wqq <929054839@qq.com>
2020-03-25 13:13:15 +0800

    新建项目提交

---
+++ demosun1/src/test/java/com/demo/demosun1/Demosun1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demosun1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demosun1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
